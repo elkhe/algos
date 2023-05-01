323a7befc08f06bf2874fd679f9475e0fedb702b
elkhe <elkhe>
2023-05-01 09:25:10 +0400

    validating sudoku by isolating group

--- valid_sudoku_hash_table.py
+++ valid_sudoku_hash_table.py
@@ -5,9 +5,9 @@ def isValidSudoku(board: List[List[str]]) -> bool:
     digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     group_types = ["row", "column", "square"]
     for type in group_types:
-        for number in range(9):
+        for group_number in range(9):
             chunk = {digit:None for digit in digits}
-            group = getGroup(board, type, number)
+            group = getGroup(board, type, group_number)
             for numb in group:
                 if chunk.get(numb):
                     return False
@@ -16,16 +16,16 @@ def isValidSudoku(board: List[List[str]]) -> bool:
     return True
 
 
-def getGroup(board: List[List[str]], type: str, number: int) -> List:
+def getGroup(board: List[List[str]], type: str, group_number: int) -> List:
     group = []
     if type == "row":
-        group = board[number]
+        group = board[group_number]
     elif type == "column":
         for i in range(9):
-            group.append(board[i][number])
+            group.append(board[i][group_number])
     elif type == "square":
-        starting_row = number // 3
-        starting_column = number % 3
+        starting_row = group_number // 3
+        starting_column = group_number % 3
         r_begin, r_end = starting_row*3, (starting_row + 1)*3
         c_begin, c_end = starting_column*3, (starting_column + 1)*3
         for i in range(r_begin, r_end):
